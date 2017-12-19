4f82a09d64b7504eca4451ffd8472577763cf9a6
Alyssa Keimach <alyssakeimach@gmail.com>
2017-12-18 16:37:48 -0800

    react-router nav links, style navbar bootstrap 4

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,16 +3,23 @@ import { Link } from "react-router-dom";
 import Login from "./Login";
 
 
-const Nav = () =>
-  <nav className="navbar navbar-inverse navbar-static-top">
-    <div className="container">
-      <ul className="nav navbar-nav">
-      <li className="nav-item">
-        <Link to="/">Brewski</Link>
-      </li>
-    </ul>
-    </div>
-  </nav>;
+const Nav = (props) => {
+  return (
+    <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+      <Link to="/"><span className="navbar-brand">Brewski</span></Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav">
+          <Link to="/home"><span className="nav-item nav-link">Home</span></Link>
+          <Link to="/reviews"><span className="nav-item nav-link">Reviews</span></Link>
+          <Link to="/history"><span className="nav-item nav-link">History</span></Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Nav;
 
