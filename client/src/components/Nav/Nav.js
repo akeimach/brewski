9b15587d46e1899fdb06000fe827e8f4ca443360
Alyssa Keimach <alyssakeimach@gmail.com>
2017-12-18 18:23:05 -0800

    improve grid structure, test components, react router

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,16 +5,27 @@ import Login from "./Login";
 
 const Nav = (props) => {
   return (
-    <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
-      <Link to="/"><span className="navbar-brand">Brewski</span></Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link to="/">
+        <span className="nav-style navbar-brand">Brewski</span>
+      </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <div className="navbar-nav">
-          <Link to="/home"><span className="nav-item nav-link">Home</span></Link>
-          <Link to="/reviews"><span className="nav-item nav-link">Reviews</span></Link>
-          <Link to="/history"><span className="nav-item nav-link">History</span></Link>
+          <Link to="/home">
+            <span className="nav-style nav-item nav-link">Home</span>
+          </Link>
+          <Link to="/reviews">
+            <span className="nav-style nav-item nav-link">Reviews</span>
+          </Link>
+          <Link to="/history">
+            <span className="nav-style nav-item nav-link">History</span>
+          </Link>
+          <Link to="/login">
+            <span className="nav-style nav-item nav-link">Login</span>
+          </Link>
         </div>
       </div>
     </nav>
