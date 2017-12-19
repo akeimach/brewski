ad8092246a6dd6cfd0f76afb7c301a1ca9ce02ad
Alferguson <alferguson@berkeley.edu>
2017-12-18 20:54:50 -0800

    schema, server, config, and some controller changes

---
+++ controllers/review_controller.js
@@ -0,0 +1,110 @@
+const db = require("../models");
+const express = require("express");
+const router = express.Router();
+
+// GET route to get a user's reviews
+router.get("/api/migraine/:id", authCheck(), function(req, res) {
+  db.Migraine.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [db.Treatment, db.Weather]
+  }).then(function(data) {
+    res.json(data);
+  })
+})
+
+// // GET route to get all migrains for a specific user
+router.get("/api/migraines", authCheck(), function(req, res) {
+  db.Migraine.findAll({
+    // display all migraines for id
+    where: {
+      UserId: req.user.id
+    },
+    include: [db.Treatment, db.Weather],
+    order: [
+      ["date", "DESC"]
+    ]
+  }).then(function(dbMigraine) {
+    res.json(dbMigraine);
+  });
+});
+
+
+// POST route to create new migraines when user clicks submit
+router.post("/api/migraines", authCheck(), function(req, res) {
+  db.Migraine.create({
+    UserId: req.user.id,
+    intensity: req.body.intensity,
+    location: req.body.location,
+    date: req.body.date,
+    trigger: req.body.trigger
+  }).then(function(dbMigraine) {
+    db.Weather.create({
+      MigraineId: dbMigraine.id,
+      temp: req.body.currentWeather.temp,
+      humidity: req.body.currentWeather.humidity,
+      precip: req.body.currentWeather.precip
+    });  
+
+    // to ADD chronic treatment
+    db.Treatment.create({
+      treatment_name: req.body.chronicTreatment.treatment_name,
+      acute: false,
+      dose: req.body.chronicTreatment.dose,
+      dose_unit: req.body.chronicTreatment.dose_unit   
+    }).then(function(dbChronicTreatment) {
+      // to ADD keys to MigraineTreatment for chronic
+      db.MigraineTreatment.create({
+        TreatmentId: dbChronicTreatment.id,
+        MigraineId: dbMigraine.id
+      });
+    });
+    // to ADD acute treatment
+    db.Treatment.create({
+      treatment_name: req.body.acuteTreatment.treatment_name,
+      acute: true,
+      dose: req.body.acuteTreatment.dose,
+      dose_unit: req.body.acuteTreatment.dose_unit  
+    }).then(function(dbAcuteTreatment) {
+      // to ADD keys to MigraineTreatment for acute
+      db.MigraineTreatment.create({
+        TreatmentId: dbAcuteTreatment.id,
+        MigraineId: dbMigraine.id
+      });
+    });
+
+    res.json(dbMigraine);
+  });
+});   
+
+// PUT route to update previous migraines
+router.put("/api/migraines/:id", authCheck(), function(req, res) {
+  db.Migraine.update(req.body,
+    {    
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: db.Treatment
+        }
+      ] 
+  }).then(function(dbMigraine) {
+    res.json(dbMigraine);
+  });  
+});  
+  
+
+// // to delete previous migraines
+router.delete("/api/migraines/:id", authCheck(), function(req, res) {
+  db.Migraine.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbMigraine) {
+    res.json(dbMigraine);
+  })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const methodOverride = require('method-override');
 const routes = require("./routes");
 const app = express();
+const db = require("./models");
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// MAYBE???
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
 // Serve up static assets
 app.use(express.static("client/build"));
-// Add routes, both API and view
 app.use(routes);
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/brewski",
-  {
-    useMongoClient: true
-  }
-);
+// // Set up promises with mongoose
+// mongoose.Promise = global.Promise;
+// // Connect to the Mongo DB
+// mongoose.connect(
+// 	process.env.MONGODB_URI || "mongodb://localhost/brewski",
+// 	{
+// 		useMongoClient: true
+// 	}
+// 	);
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// // Start the API server
+// app.listen(PORT, function() {
+// 	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// });
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: false }).then(function() {
+	app.listen(PORT, function() {
+		console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+	});
 });
