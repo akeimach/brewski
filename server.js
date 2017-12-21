60fa68de5fb3e4614ef536fd22bc6d8b914e03d8
Alferguson <alferguson@berkeley.edu>
2017-12-21 14:22:54 -0800

    eh

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const routes = require("./routes");
 const beerRoute = require("./controllers/beer_controller.js");
 const usersRoute = require("./controllers/user_controller.js");
 const reviewRoute = require("./controllers/review_controller.js");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const router = express.Router();
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,10 +33,16 @@ app.use(express.static("client/build"));
 app.use(routes);
 
 // routes
-const router = express.Router();
 router.use("/", beerRoute);
 router.use("/", usersRoute);
 router.use("/", reviewRoute);
+const root = { hello: () => 'Hello world!' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
