ed6677918292f31341a885dce4d7c5f4a4f531ed
Alferguson <alferguson@berkeley.edu>
2017-12-21 19:29:43 -0800

    still aint working

--- routes/api/rateBeerRoutes.js
+++ routes/api/rateBeerRoutes.js
@@ -1,29 +1,56 @@
 const router = require("express").Router();
 const request = require("request");
-
-// Matches with "/api/ratebeer"
-router.post("/", (req, res) => {
-
-  var headers = {
+const headers = {
     "content-type": "application/json",
     "accept": "application/json",
     "x-api-key": process.env.RATE_BEER
-  };
+};
 
-  var dataString = '{"query":"query { beer(id: 4934) { id name }}","variables":"{}"}';
+const dataString = '{"query":"query { beer(id: 4934) { id name }}","variables":"{}"}';
 
-  var options = {
-    url: "https://api.r8.beer/v1/api/graphql/",
-    method: "POST",
-    headers: headers,
-    body: dataString
-  };
+const options = {
+  url: "https://api.r8.beer/v1/api/graphql/",
+  method: "POST",
+  headers: headers,
+  body: dataString
+};
+// // Matches with "/api/ratebeer"
+// router.post("/", (req, res) => {
 
-  request(options, (req, res) => {
-    console.log(res.body);
-  });
 
+//   request(options, (req, res) => {
+//     console.log(res.body);
+//   });
+
+// });
+
+// module.exports = router;
+
+const { createApolloFetch } = require('apollo-fetch');
+
+const fetch = createApolloFetch({
+  uri: 'https://api.r8.beer/v1/api/graphql/',
 });
 
+fetch({
+  headers: headers,
+    body: dataString
 
-module.exports = router;
+
+}).then(res => {
+  console.log(res);
+});
+
+// You can also easily pass variables for dynamic arguments
+fetch({
+    headers: headers,
+
+  query: `query {
+    beer(id: 4934) {
+      name
+    }
+  }
+`
+}).then(res => {
+  console.log(res);
+});
