c45d1b4dcf84ce33bacb3bd50cbd2ba174379d1e
Patrick Luu <pluu@Minhs-MBP.T-mobile.com>
2017-12-28 08:25:38 -0800

    add field to post review

--- client/src/App.js
+++ client/src/App.js
@@ -91,6 +91,8 @@ class App extends React.Component {
     }
   };
 
+
+
   render() {
     return (
       <div>
--- client/src/pages/Reviews.js
+++ client/src/pages/Reviews.js
@@ -1,15 +1,49 @@
 import React from "react";
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Form, FormGroup, Col, ControlLabel, FormControl } from 'react-bootstrap';
+
 
 
 const ReviewDisplay = (props) => {
+
+
 
   return (
     <div>
       <br />
       <Jumbotron>
         <h1>Reviews You've Written</h1>
-        <p>Coming soon</p>
+
+         <Form horizontal>
+		    <FormGroup controlId="formHorizontalEmail">
+		      <Col componentClass={ControlLabel} sm={2}>
+		        Name
+		      </Col>
+		      <Col sm={10}>
+		        <FormControl type="name" placeholder="John" />
+		      </Col>
+		    </FormGroup>
+
+		    <FormGroup controlId="formHorizontalPassword">
+		      <Col componentClass={ControlLabel} sm={2}>
+		        Beer Score
+		      </Col>
+		      <Col sm={10}>
+		        <FormControl type="beerScore" placeholder="5" />
+		      </Col>
+		    </FormGroup>
+
+		    <FormGroup controlId="formHorizontalPassword">
+		      <Col componentClass={ControlLabel} sm={2}>
+		        Beer Review
+		      </Col>
+		      <Col sm={10}>
+		        <FormControl componentClass="textarea" type="beerReview" placeholder="Review" />
+		      </Col>
+		    </FormGroup>
+
+		 </Form>
+
+
       </Jumbotron>
     </div>
   );
