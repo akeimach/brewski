22117c012cb69015ddd22e63fbf66dde4073f20b
Alyssa Keimach <alyssakeimach@gmail.com>
2017-12-30 09:57:10 -0800

    rebuild with working oauth

--- client/src/components/Login/OAuth.js
+++ client/src/components/Login/OAuth.js
@@ -4,14 +4,9 @@ import { GoogleLogin } from 'react-google-login-component';
 
 export default class OAuth extends React.Component {
 
-  constructor (props) {
-    super(props);
-  }
-
   responseGoogle = (googleUser) => {
     var id_token = googleUser.getAuthResponse().id_token;
     console.log({accessToken: id_token});
-    this.props.toggleModal;
   }
 
   render () {
