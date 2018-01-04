cca53399b8475da3464c3efc771c1c1592dd7a0c
Alyssa Keimach <alyssakeimach@gmail.com>
2018-01-03 18:12:54 -0800

    prune unused packages, clean up console logs

--- client/src/components/Login/OAuth.js
+++ client/src/components/Login/OAuth.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { GoogleLogin } from 'react-google-login';
-import API from '../../utils/API.js';
+import React from "react";
+import { GoogleLogin } from "react-google-login";
+import API from "../../utils/API.js";
 
 export default class OAuth extends React.Component {
 
   responseGoogle = (googleUser) => {
-    
     let id_token = googleUser.getAuthResponse().id_token;
-    console.log(googleUser.getAuthResponse());
-    console.log(id_token);
     API.postUser({
       googleId: id_token
     })
     .then(res => {
-      console.log("User is ", res.data.id);
       localStorage.setItem("userId", res.data.id);
     });
   }
